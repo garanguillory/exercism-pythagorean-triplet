7a0ccf9531865ffb82e89f49c07ec4d9863e5ad9
Garan Guillory <garanguillory@gmail.com>
2016-02-16 13:20:22 -0700

    First two tests passing

---
+++ pythagorean-triplet.js
@@ -0,0 +1,14 @@
+var Triplet = function(s1,s2,s3){
+
+	this.sum = function(){
+			return s1 + s2 + s3;
+	};
+	this.product = function(){
+			return s1 * s2 * s3;
+	};
+
+
+};
+
+
+module.exports = Triplet;
